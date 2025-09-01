a8843968cf0a255c3e627f50e453f73cefa9584f
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-01 15:44:44 +0300

    [Feature] Create SpotifyHomeView

---
+++ SwiftUIPractice/Spotify/View/SpotifyHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyHomeView.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 01.09.2025.
+//
+
+import SwiftUI
+
+struct SpotifyHomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SpotifyHomeView()
+}
