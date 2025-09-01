94f9a0e07d75ee18ff0b5dfbe9fdd84a0a2fe520
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-01 15:39:57 +0300

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
