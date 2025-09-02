bf50586b44bd727443f2d299e9d4b0a927af1fb6
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-02 13:59:38 +0300

    [Feature] Add Packages

---
+++ SwiftUIPractice/Shared/ImageLoaderView.swift
@@ -0,0 +1,16 @@
+//
+//  ImageLoaderView.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 02.09.2025.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    
+    
+    var body: some View {
+        Text("ImageLoaderView")
+    }
+}
