e1ddaa502369a992076e5205d6fdda69751e7201
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-02 13:47:24 +0300

    [Featute] Create User Model

---
+++ SwiftUIPractice/Shared/User.swift
@@ -0,0 +1,36 @@
+//
+//  User.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 02.09.2025.
+//
+
+import Foundation
+
+struct UserArray: Codable {
+    let users: [User]
+    let total, skip, limit: Int
+}
+
+struct User: Codable, Identifiable {
+    let id: Int
+    let firtName, lastName: String
+    let email, phone, username, password: String
+    let image: String
+    let height, weight: Double
+    
+    static var mock: User {
+        User(
+            id: 123,
+            firtName: "Valentyma",
+            lastName: "Kharkova",
+            email: "gerry29072001@gmail.com",
+            phone: "+380994477653",
+            username: "Val",
+            password: "jabhdhasjbfhj",
+            image: Constants.randomImage,
+            height: 180,
+            weight: 80
+        )
+    }
+}
