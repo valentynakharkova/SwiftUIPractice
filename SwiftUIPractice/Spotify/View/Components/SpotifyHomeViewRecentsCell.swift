8d3c2b459ed4e1e9867b1456fc607b6246e3ac9e
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-02 19:00:45 +0300

    [Feature] Created SpotifyHomeViewRecentsCell

--- SwiftUIPractice/Spotify/View/Components/SpotifyHomeViewRecentsCell.swift
+++ SwiftUIPractice/Spotify/View/Components/SpotifyHomeViewRecentsCell.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SpotifyHomeViewRecentsCell: View {
+    
+    var imageName: String = Constants.randomImage
+    var title: String = "Some random title for this View"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 16) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: 55, height: 55)
+            
+            Text(title)
+                .font(.callout)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+        }
+        .padding(.trailing, 8)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .themeColors(isSelected: false)
+        .cornerRadius(6)
     }
 }
 
 #Preview {
-    SpotifyHomeViewRecentsCell()
+    ZStack {
+        Color.spotifyBlack.ignoresSafeArea()
+        VStack {
+            HStack {
+                SpotifyHomeViewRecentsCell()
+                SpotifyHomeViewRecentsCell()
+            }
+            HStack {
+                SpotifyHomeViewRecentsCell()
+                SpotifyHomeViewRecentsCell()
+            }
+        }
+    }
 }
