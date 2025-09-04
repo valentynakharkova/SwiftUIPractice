72f5cfd9defa0bdbe842dc8a3a719674ad156f5f
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-04 15:35:19 +0300

    [Feature] Added Netflix Colorset

---
+++ SwiftUIPractice/Netflix/Views/Components/HomeView/FilterBarView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterBarView.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 04.09.2025.
+//
+
+import SwiftUI
+
+struct FilterBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterBarView()
+}
