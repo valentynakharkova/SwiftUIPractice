e07c4abe1d673eb90feb073a24610bc64d073cd4
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-04 13:55:59 +0300

    [Feature] Created BumbleCardView

--- SwiftUIPractice/Bumble/Views/Components/HomeView/CardView.swift
+++ SwiftUIPractice/Bumble/Views/Components/HomeView/CardView.swift
@@ -8,8 +8,230 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    var user: User = .mock
+    var onSuperlikePressed: (() -> Void)? = nil
+    var onXmarkPressed: (() -> Void)? = nil
+    var onChackmarkPressed: (() -> Void)? = nil
+    var onSendAComplimentPressed: (() -> Void)? = nil
+    var onHideAndReportPressed: (() -> Void)? = nil
+    
+    @State private var cardFrame: CGRect = .zero
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack(spacing: 0 ) {
+                headerCell
+                    .frame(height: cardFrame.height)
+                
+                aboutMeSection
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 24)
+                
+                
+                myInterestSection
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 24)
+
+                ForEach(user.images, id: \.self) { image in
+                    ImageLoaderView(urlString: image)
+                        .frame(height: cardFrame.height)
+                }
+                locationSection
+                   .padding(.horizontal, 24)
+                   .padding(.vertical, 24)
+                
+                footerSection
+                    .padding(.top, 60)
+                    .padding(.bottom, 60)
+                    .padding(.horizontal, 32)
+                    
+
+            }
+            
+        }
+        .background(.bumbleBackgroundYellow)
+        .overlay(alignment: .bottomTrailing, content: {
+            superlikeButton
+                .padding(24)
+        })
+        .cornerRadius(32)
+        .readingFrame { frame in
+            cardFrame = frame
+        }
+    }
+    
+    private func sectionTitle(title: String) -> some View {
+        Text(title)
+            .font(.body)
+            .foregroundStyle(.bumbleGray )
+    }
+    
+    
+    private var headerCell: some View {
+        ZStack(alignment: .bottomLeading) {
+            ImageLoaderView(urlString: user.image)
+            
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("\(user.firstName), \(user.age)")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                HStack(spacing: 4) {
+                    Image(systemName: "suitcase")
+                    Text(user.work)
+                    
+                }
+                HStack(spacing: 4) {
+                    Image(systemName: "graduationcap")
+                    Text(user.education)
+                }
+                
+                HeartView()
+                    .onTapGesture {
+                        
+                    }
+            }
+            .padding(24)
+            .font(.callout)
+            .fontWeight(.medium)
+            .foregroundStyle(.bumbleWhite)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background {
+                LinearGradient(
+                    colors: [
+                        .bumbleBlack.opacity(0),
+                        .bumbleBlack.opacity(0.6),
+                        .bumbleBlack.opacity(0.6)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            }
+            
+            
+                
+        }
+    }
+    
+    private var aboutMeSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            sectionTitle(title: "About me")
+            Text(user.aboutMe)
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundStyle(.bumbleBlack)
+            
+            HStack(spacing: 0) {
+                HeartView()
+                
+                Text("Send a compliment")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        onSendAComplimentPressed?()
+                    }
+            }
+            .padding([.horizontal, .trailing], 8)
+            .background(.bumbleYellow)
+            .cornerRadius(32)
+                
+                
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var myInterestSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            VStack(alignment: .leading, spacing: 8) {
+                sectionTitle(title: "My basics")
+                InterestPillGridView(interests: user.basics)
+            }
+            VStack(alignment: .leading, spacing: 8) {
+                sectionTitle(title: "My interests")
+                InterestPillGridView(interests: user.interests)
+            }
+
+
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var locationSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(spacing: 8) {
+                Image(systemName: "mappin.and.ellipse.circle.fill")
+                Text("\(user.firstName)'s Location")
+                    
+            }
+            .font(.body)
+            .fontWeight(.medium)
+            .foregroundStyle(.bumbleGray)
+            
+            Text("10 miles away")
+                .font(.headline)
+                .foregroundStyle(.bumbleBlack)
+            
+            InterestPillView(iconName: nil,  emoji: "🇺🇸", text: "Lives in New York, NY")
+                
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var footerSection: some View {
+        VStack(alignment: .center, spacing: 24) {
+            HStack(spacing: 0) {
+                Circle()
+                    .fill(.bumbleYellow)
+                    .overlay(content: {
+                        Image(systemName: "xmark")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    })
+                    .frame(width: 60, height: 60)
+                    .onTapGesture {
+                        onXmarkPressed?()
+                    }
+                
+                Spacer(minLength: 0)
+                
+                Circle()
+                    .fill(.bumbleYellow)
+                    .overlay(content: {
+                        Image(systemName: "checkmark")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    })
+                    .frame(width: 60, height: 60)
+                    .onTapGesture {
+                        onChackmarkPressed?()
+                    }
+            }
+            
+            Text("Hide and Report")
+                .font(.headline)
+                .foregroundStyle(.bumbleGray)
+                .padding(8)
+                .background(.black.opacity(0.001))
+                .onTapGesture {
+                    onHideAndReportPressed?()
+                }
+        }
+    }
+    
+    private var superlikeButton: some View {
+        Image(systemName: "hexagon.fill")
+            .foregroundStyle(.bumbleYellow)
+            .font(.system(size: 60))
+            .overlay {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.bumbleBlack)
+                    .font(.system(size: 30, weight: .medium))
+            }
+            .onTapGesture {
+                onSuperlikePressed?()
+            }
     }
 }
 
