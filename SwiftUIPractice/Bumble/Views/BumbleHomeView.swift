b490a8c6a804f74da1476713604c668454a87e3d
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-04 13:02:45 +0300

    [Feature] Created header in BumbleHomeView

--- SwiftUIPractice/Bumble/Views/BumbleHomeView.swift
+++ SwiftUIPractice/Bumble/Views/BumbleHomeView.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 struct BumbleHomeView: View {
     
-    
+    @StateObject private var viewModel = BumbleHomeViewModel()
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.bumbleWhite.ignoresSafeArea()
+            
+            VStack(spacing: 12) {
+                header
+            }
+        }
+    }
+    
+    private var header: some View {
+        HStack(spacing: 0) {
+            HStack(spacing: 0) {
+                Image(systemName: "line.horizontal.3")
+                    .padding(8)
+                    .background(.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+                Image(systemName: "arrow.uturn.left")
+                    .padding(8)
+                    .background(.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("bumble")
+                .font(.title)
+                .foregroundStyle(.bumbleYellow)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(.black.opacity(0.001))
+                .onTapGesture {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumbleBlack)
     }
 }
 
