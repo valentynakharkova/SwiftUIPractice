a9d999d1b16c8e6f2808c17b21655ee0bc9c4848
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-04 12:52:56 +0300

    [Feauture] Created BumbleHomeViewModel

---
+++ SwiftUIPractice/Bumble/ViewModels/BumbleHomeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  BumbleHomeViewModel.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 04.09.2025.
+//
+
+import Foundation
+
+class BumbleHomeViewModel: ObservableObject {
+    
+    @Published var filters: [String] = ["Everyone", "Trending"]
+    @Published var allUsers: [User] = []
+    @Published var selectedIndex: Int = 1
+    @Published var cardOffset: [Int: Bool] = [:] // // UserID : Direction Right = TRUE, Left = FALSE
+    @Published var currentSwipeOffset: CGFloat = 0
+    
+    func getData() async {
+        
+        guard allUsers.isEmpty else { return }
+        
+        do {
+            allUsers = try await DatabaseHelper().getUsers()
+        } catch {
+            
+        }
+    }
+    
+}
---
+++ SwiftUIPractice/Bumble/Views/BumbleHomeView.swift
@@ -0,0 +1,21 @@
+//
+//  BumbleHomeView.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 04.09.2025.
+//
+
+import SwiftUI
+
+struct BumbleHomeView: View {
+    
+    
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BumbleHomeView()
+}
