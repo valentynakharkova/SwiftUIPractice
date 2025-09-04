3ff83032a457711f47f735a6f3abff74fdf67b78
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-04 14:36:00 +0300

    [Feature] Added data to User

--- SwiftUIPractice/Bumble/ViewModels/BumbleHomeViewModel.swift
+++ SwiftUIPractice/Bumble/ViewModels/BumbleHomeViewModel.swift
@@ -9,10 +9,11 @@ import Foundation
 
 class BumbleHomeViewModel: ObservableObject {
     
+
     @Published var filters: [String] = ["Everyone", "Trending"]
     @Published var allUsers: [User] = []
     @Published var selectedIndex: Int = 1
-    @Published var cardOffset: [Int: Bool] = [:] // // UserID : Direction Right = TRUE, Left = FALSE
+    @Published var cardOffsets: [Int: Bool] = [:] // // UserID : Direction Right = TRUE, Left = FALSE
     @Published var currentSwipeOffset: CGFloat = 0
     
     func getData() async {
--- SwiftUIPractice/SharedModels/User.swift
+++ SwiftUIPractice/SharedModels/User.swift
@@ -15,6 +15,7 @@ struct UserArray: Codable {
 struct User: Codable, Identifiable {
     let id: Int
     let firstName, lastName: String
+    let age: Int
     let email, phone, username, password: String
     let image: String
     let height, weight: Double
@@ -24,6 +25,7 @@ struct User: Codable, Identifiable {
             id: 123,
             firstName: "Valentyna",
             lastName: "Kharkova",
+            age: 31,
             email: "valentyna@gmail.com",
             phone: "555-13-15",
             username: "Val",
@@ -32,5 +34,35 @@ struct User: Codable, Identifiable {
             height: 180,
             weight: 80
         )
+    }
+    
+    var work: String {
+        "Worker as Some Job"
+    }
+    var education: String {
+        "Graduate Degree"
+    }
+    var aboutMe: String {
+        "This is a sentence about me that will look good on my profile!"
+    }
+    var basics: [UserInterest] {
+        [
+            UserInterest(iconName: "ruler", emoji: nil, text: "\(height)"),
+            UserInterest(iconName: "graduationcap", emoji: nil, text: education),
+            UserInterest(iconName: "wineglass", emoji: nil, text: "Socially"),
+            UserInterest(iconName: "moon.stars.fill", emoji: nil, text: "Cancer"),
+        ]
+    }
+    var interests: [UserInterest] {
+        [
+            UserInterest(iconName: nil, emoji: "👟", text: "Running"),
+            UserInterest(iconName: nil, emoji: "🏋️‍♂️", text: "Gym"),
+            UserInterest(iconName: nil, emoji: "🎧", text: "Music"),
+            UserInterest(iconName: nil, emoji: "🥘", text: "Cooking"),
+        ]
+    }
+    
+    var images: [String] {
+        ["https://picsum.photos/500/500", "https://picsum.photos/600/600", "https://picsum.photos/700/700"]
     }
 }
