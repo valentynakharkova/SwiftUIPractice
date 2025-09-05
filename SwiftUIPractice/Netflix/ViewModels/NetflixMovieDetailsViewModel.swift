1a4535721b9ca5624dccb5aa55ed5f91b034e26a
Valentyna Kharkova <gerry29072001@gmail.com>
2025-09-05 12:24:13 +0300

    [Feature] Created NetflixMovieDetailsViewModel

---
+++ SwiftUIPractice/Netflix/ViewModels/NetflixMovieDetailsViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  NetflixMovieDetailsViewModel.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 05.09.2025.
+//
+
+import Foundation
+
+class NetflixMovieDetailsViewModel: ObservableObject {
+    
+    @Published var progress: Double = 0.5
+    @Published var isMyList: Bool = true
+    @Published var products: [Product] = []
+    
+    func getData() async {
+        
+        guard products.isEmpty else { return }
+         
+        do {
+            products = try await DatabaseHelper().getProducts()
+        } catch  {
+            
+        }
+    }
+}
---
+++ SwiftUIPractice/Netflix/Views/NetflixMovieDetailsView.swift
@@ -0,0 +1,25 @@
+//
+//  NetflixMovieDetailsView.swift
+//  SwiftUIPractice
+//
+//  Created by Valentyna Kharkova on 05.09.2025.
+//
+
+import SwiftUI
+
+struct NetflixMovieDetailsView: View {
+    
+    @StateObject private var viewModel = NetflixMovieDetailsViewModel()
+    
+    var body: some View {
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            
+            
+        }
+    }
+}
+
+#Preview {
+    NetflixMovieDetailsView()
+}
